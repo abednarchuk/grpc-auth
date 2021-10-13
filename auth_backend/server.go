812cf1f59c2bf763bc68ab6cc3d38fd2d4e2f02f
Andrii Bednarchuk <andrii.bednarchuk@gmail.com>
2021-10-13 10:24:06 +0200

    Connected mongo and backend inside container

--- auth_backend/server.go
+++ auth_backend/server.go
@@ -2,20 +2,47 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log"
+	"net"
 
 	"github.com/abednarchuk/grpc_auth/auth_backend/authpb"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"google.golang.org/grpc"
 )
 
 type server struct {
+	mongoClient *mongo.Client
 	authpb.UnimplementedSignupServiceServer
 }
 
 func (*server) SignUp(ctx context.Context, req *authpb.SignupRequest) (*authpb.SignupResponse, error) {
+	log.Println("SignUp func was invoked with req: ", req)
 
 	return &authpb.SignupResponse{}, nil
 }
 
 func main() {
-	fmt.Println("test")
+	clientOptions := options.Client().ApplyURI("mongodb://mongo")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("Ping successfull!")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	s := grpc.NewServer()
+	authpb.RegisterSignupServiceServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalln(err)
+	}
 }
